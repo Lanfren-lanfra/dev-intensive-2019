e68cc30c0ee1f3590892900120bb38ddff7cf91e
Olga <Olga@me.dot>
2019-09-20 22:13:52 +0300

    ex#6: transliteration

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Utils.kt
@@ -10,8 +10,8 @@ object Utils {
         var fn = names?.getOrNull(0)
         var ln = names?.getOrNull(1)
 
-        fn=emptyToNull(fn)
-        ln=emptyToNull(ln)
+        fn = emptyToNull(fn)
+        ln = emptyToNull(ln)
 
         return Pair(fn, ln)
 
@@ -20,21 +20,82 @@ object Utils {
     /**
      * возвращающий строку с первыми буквами имени и фамилии в верхнем регистре
      */
-    fun toInitials(firstName: String?, lastName:String?):String? {
+    fun toInitials(firstName: String?, lastName: String?): String? {
         var f = emptyToNull(firstName)?.substring(0, 1)?.toUpperCase()
         var l = emptyToNull(lastName)?.substring(0, 1)?.toUpperCase()
         return when {
             f == null && l == null -> null
             f == null -> l
-            l ==null -> f
+            l == null -> f
             else -> "${f}${l}"
         }
     }
 
+    fun transliteration(payload: String, divider: String = " "): String? {
+
+        if (payload.isBlank()) return null
+
+        var translated = ""
+
+        for (pchar in payload) {
+            translated += transliterateLitera(pchar)
+        }
+
+        return translated.replace(" ", divider)
+    }
+
+    private fun transliterateLitera(pchar: Char): String {
+        var uppercaseFlag = pchar.isUpperCase()
+        var ch = if (uppercaseFlag) pchar.toLowerCase() else pchar
+
+        var translated = when (ch) {
+            'а' -> "a"
+            'б' -> "b"
+            'в' -> "v"
+            'г' -> "g"
+            'д' -> "d"
+            'е' -> "e"
+            'ё' -> "e"
+            'ж' -> "zh"
+            'з' -> "z"
+            'и' -> "i"
+            'й' -> "i"
+            'к' -> "k"
+            'л' -> "l"
+            'м' -> "m"
+            'н' -> "n"
+            'о' -> "o"
+            'п' -> "p"
+            'р' -> "r"
+            'с' -> "s"
+            'т' -> "t"
+            'у' -> "u"
+            'ф' -> "f"
+            'х' -> "h"
+            'ц' -> "c"
+            'ч' -> "ch"
+            'ш' -> "sh"
+            'щ' -> "sh'"
+            'ъ' -> ""
+            'ы' -> "i"
+            'ь' -> ""
+            'э' -> "e"
+            'ю' -> "yu"
+            'я' -> "ya"
+            else -> ch.toString()
+        }
+
+        return if (uppercaseFlag) translated.capitalize() else translated
+
+    }
+
+
+
+
     private fun emptyToNull(str: String?): String? {
-        when{
+        when {
             str.equals(null) || str!!.isBlank() -> return null
-             else -> return str
+            else -> return str
         }
     }
 }
