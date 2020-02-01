9a246ef17d5cd5609c31e872015ea008feea63cc
Olga <Olga@me.dot>
2020-02-01 23:10:48 +0300

    ex: isOpenKeyboard

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,7 +1,11 @@
 package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
 import android.inputmethodservice.InputMethodService
+import android.util.TypedValue
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,4 +15,24 @@ fun Activity.hideKeyboard(){
     val imm:InputMethodManager = this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     val textMessage: EditText = et_message
     imm.hideSoftInputFromWindow(textMessage.windowToken, 0)
+}
+
+fun Context.convertDpToPx(dp: Float): Float {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        this.resources.displayMetrics
+    )
+}
+fun Activity.isKeyboardOpen(): Boolean {
+    val visibleBounds = Rect()
+    val rootView = findViewById<View>(android.R.id.content)
+    rootView.getWindowVisibleDisplayFrame(visibleBounds)
+    val heightDiff = rootView.height - visibleBounds.height()
+    val marginOfError = Math.round(this.convertDpToPx(50F))
+    return heightDiff > marginOfError
+}
+
+fun Activity.isKeyboardClosed(): Boolean {
+    return !this.isKeyboardOpen()
 }
